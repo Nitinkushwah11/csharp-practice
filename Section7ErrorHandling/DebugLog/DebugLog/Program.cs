6681e28405fd45ba48ac31ad1eee7b362f7bb529
Nitinkushwah11 <nitinkush9587@gmail.com>
2026-07-06 16:56:03 +0530

    [Nitin Kushwah] : Added Section7 ErrorHandling

---
+++ Section7ErrorHandling/DebugLog/DebugLog/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace TryCatchExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            Debug.WriteLine("Main Method is calling....");
+
+            try
+            {
+                Console.WriteLine("Please enter a number");
+                Debug.WriteLine("We are inside the try block....");
+                int num1 = int.Parse(Console.ReadLine());
+                //int num1 = 0;
+                int num2 = 2;
+                result = num2 / num1;
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Error: " + ex.StackTrace);
+                Debug.WriteLine("Now!, We are inside the catch block....");
+            }
+            finally
+            {
+                // Code to cleanup or finalize
+                // ideal for cleaning up resources,
+                // like closing file streams or database connections.
+                Debug.WriteLine("Now!, We are inside the finally block....");
+                Console.WriteLine("This always executes");
+            }
+            Debug.WriteLine("......We out of the block....");
+            Console.WriteLine("Result: " + result);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Section7ErrorHandling/TryCatchExceptions/TryCatchExceptions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace TryCatchExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            try
+            {
+                Console.WriteLine("Please enter a number");
+                int num1 = int.Parse(Console.ReadLine());
+                //int num1 = 0;
+                int num2 = 2;
+                result = num2 / num1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Code to cleanup or finalize
+                // ideal for cleaning up resources,
+                // like closing file streams or database connections.
+
+                Console.WriteLine("This always executes");
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadKey();
+
+            
+
+        }
+    }
+}
